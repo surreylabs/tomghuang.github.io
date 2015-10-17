03bc54b9e20ee4e40d128325f1c8c322b1197de8
Tom G. Huang <tomghuang@gmail.com>
2015-10-17 10:13:29 -0700

    Create update.py

---
+++ assets/update.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+from urllib import urlretrieve
+
+url = "https://gist.githubusercontent.com/surreylabs/60201ecb1b04de9b9f5e/raw/6cb4ffe41b093c52504533081b02dfe5c2d4c020/test"
+
+def report_hook(block_num, block_size, total_size):
+    readsofar = block_num * block_size
+    if total_size > 0:
+        percent = readsofar * 1e2 / total_size
+        s = "\r%5.1f%% %*d / %d" % (
+                percent, len(str(total_size)), readsofar, total_size)
+        sys.stderr.write(s)
+        if readsofar >= total_size:
+            sys.stderr.write("\n")
+    else:
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+
+if __name__ == "__main__":
+    local_file_path = os.path.realpath(__file__)
+    urlretrieve(url, local_file_path, report_hook)
